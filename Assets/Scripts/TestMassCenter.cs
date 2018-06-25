e3aaced0e588e14f255e7362707e69e97feb2b77
Marcello <probobbo@gmail.com>
2018-06-25 16:31:03 +0200

    test a caso

---
+++ Assets/Scripts/TestMassCenter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMassCenter : MonoBehaviour
+{
+
+	public Transform World,CenterOfMass;
+	public float GravityIntensity, MoveSpeed;
+	private Rigidbody RB;
+	private float Hor, Ver;
+	private Vector3 MovementDirection;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		RB = GetComponent<Rigidbody>();
+		RB.centerOfMass = CenterOfMass.transform.position;
+	}
+
+	private void Update()
+	{
+		Hor = Input.GetAxis("Horizontal");
+		Ver = Input.GetAxis("Vertical");
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		Physics.gravity = (World.position - transform.position).normalized * GravityIntensity;
+		MovementDirection = (transform.forward * Ver) + (transform.right * Hor);
+		RB.AddForce(MovementDirection * Time.fixedDeltaTime * MoveSpeed, ForceMode.VelocityChange);
+	}
+}
